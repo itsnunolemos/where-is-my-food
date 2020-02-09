2c2e88a530f638c0d63db86e0b9befdbec4c6315
Nuno Lemos <itsnunolemos@gmail.com>
2020-02-09 23:43:42 +0000

    Updates

---
+++ precache-manifest.6a0954cf5e5906fecc397f514bc40008.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac19accb452ec336008b1c8612e91bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e15632d2e54f0290f522",
+    "url": "/static/css/2.699c7d85.chunk.css"
+  },
+  {
+    "revision": "9b538777ae95fd5efd4c",
+    "url": "/static/css/main.311cb43e.chunk.css"
+  },
+  {
+    "revision": "e15632d2e54f0290f522",
+    "url": "/static/js/2.9be8bbdf.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.9be8bbdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b538777ae95fd5efd4c",
+    "url": "/static/js/main.eeb9bfef.chunk.js"
+  },
+  {
+    "revision": "d4690777d0a93e603c5d",
+    "url": "/static/js/runtime-main.5659baaf.js"
+  }
+]);
